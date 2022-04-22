bb905da0ef67c624a314b19a38e6ceb50b5744cc
hinal <hinalpatel3776@gmail.com>
2022-04-22 14:28:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/external_practical/Person.kt
@@ -0,0 +1,12 @@
+package com.example.external_practical
+
+data class Person (var pr_fname:String,var pr_lname:String,var pr_age:Int)
+{
+    var pr_id:Int=0
+    constructor(pr_id:Int,pr_fname:String,pr_lname: String,pr_age: Int):this(pr_fname, pr_lname, pr_age)
+    {
+        this.pr_id = pr_id
+    }
+}
+
+
